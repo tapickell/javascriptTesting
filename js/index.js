0c400f75ddff22f73a2269c5238f3f09f220f368
Todd Pickell <toddpickell@me.com>
2013-04-18 15:37:09 -0500

    got button working for a bit now functions need to work wioth timer

--- js/index.js
+++ js/index.js
@@ -36,6 +36,7 @@ var app = {
     // function, we must explicity call 'app.receivedEvent(...);'
     onDeviceReady: function() {
         app.receivedEvent('deviceready');
+        document.getElementById("start_button").onclick = onStartButtonPress;
     },
 
     // Update DOM on a Received Event
@@ -51,9 +52,47 @@ var app = {
     }
 };
 
+var total_time = 0;
+var pom_time = 25;
+var break_time = 5;
+var start_time;
 
 
 function onStartButtonPress() {
+    start_timer();
+}
+
+function start_timer() {
+    //disable button or set to pause here
+    start_time = Date.now();
+    comsole.log("START_TIME: " + start_time.getTime());
+    //need way to start break when pom is done
+    timer(pom_time);
+    timer(break_time);
+}
+
+function timer(stage_time) {
+    var current_time = Date.now();
+
+    total_time += current_time.getTime() - start_time.getTime();
+    start_time.getTime() = current_time.getTime();
+
+    var time_to_display = (total_time / 1000) + start_time.getTime();
+    updateLabel(time_to_display);
+
+    console.log("TOTAL_TIME: " + total_time);
+
+    if ((total_time / 1000) >= stage_time) {
+        stopTimer();
+        return;
+    };
+}
+
+function updateLabel(time_to_display) {
     var my_label = document.getElementById("label");
-    my_label.innerHTML = "<h1>onStartButtonPress called</h1>";
+    my_label.innerHTML = "<h1>" + time_to_display + "</h1>";
+}
+
+function stopTimer() {
+    total_time = 0;
 }
